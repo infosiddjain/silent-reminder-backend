8842edfa4857292cec48cf1d513522610300eedf
Siddharth Jain <siddharthjain@Siddharths-MacBook-Air.local>
2026-07-04 13:52:26 +0530

    feat: delete reminder api

--- controllers/reminder/create.js
+++ controllers/reminder/create.js
@@ -58,3 +58,37 @@ export const getReminderList = async (req, res) => {
     });
   }
 };
+
+export const deleteReminder = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const reminder = await REMINDER.findOne({
+      _id: id,
+      userId: req.user.id,
+      isDeleted: false,
+    });
+
+    if (!reminder) {
+      return res.status(404).json({
+        success: false,
+        data: null,
+        message: "Reminder not found.",
+      });
+    }
+
+    ((reminder.isDeleted = true), await reminder.save());
+
+    return res.status(200).json({
+      success: true,
+      data: null,
+      message: "Reminder deleted successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: error.message,
+    });
+  }
+};
--- routes/reminder/index.js
+++ routes/reminder/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createReminder,
+  deleteReminder,
   getReminderList,
 } from "../../controllers/reminder/create.js";
 import { verifyToken } from "../../helpers/authMiddleware.js";
@@ -9,5 +10,5 @@ const router = express.Router();
 
 router.post("/create-reminder", verifyToken, createReminder);
 router.get("/list-reminder", verifyToken, getReminderList);
-
+router.delete("/delete-reminder/:id", verifyToken, deleteReminder);
 export default router;
